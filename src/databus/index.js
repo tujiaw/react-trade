e3fe610f41f0a30e02dfd68130b861088fad6fb5
tujiaw <tujiaw@163.com>
2018-03-15 17:47:00 +0800

    add

--- src/databus/index.js
+++ src/databus/index.js
@@ -8,124 +8,9 @@
   else
     global["cbus"] = factory(global.cbusCore, global.cbusCommand);
 })(this, function (databus, cbusCommand) {
-  const {
-    AppList,
-    ProtoFileList
-  } = cbusCommand;
-  const commandCache = {}
-  // const msgExpress = {
-  //   KEY_UUID: 1,KEY_AUTH: 2,KEY_ADDR: 3,KEY_NAME: 4,KEY_TYPE: 5,KEY_GROUP: 6,
-  //   KEY_IP: 7,KEY_STARTTIME: 8,KEY_LOGINTIME: 9,KEY_SERVICE: 10,KEY_HBTIME: 20,
-  //   KEY_CPU: 21,KEY_TOPMEM: 22,KEY_MEM: 23,KEY_CSQUEUE: 24,KEY_CRQUEUE: 25,
-  //   KEY_QUEUELENGTH: 29,KEY_RECVREQUEST: 30,KEY_SENTREQUEST: 31,KEY_RECVRESPONSE: 32,
-  //   KEY_SENTRESPONSE: 33,KEY_RECVPUBLISH: 34,KEY_SENTPUBLISH: 35,KEY_RECVREQUESTB: 36,
-  //   KEY_SENTREQUESTB: 37,KEY_RECVRESPONSEB: 38,KEY_SENTRESPONSEB: 39,KEY_RECVPUBLISHB: 40,
-  //   KEY_SENTPUBLISHB: 41,KEY_LOGLEVEL: 61,KEY_LOGDATA: 62, KEY_TIME: 11 ,KEY_BROKER:12
-  // };
-
-  /**
-   * 判断是否是数组
-   * 
-   * @param {any} obj 
-   * @returns bool
-   */
-  function isArray(obj) {
-    return Object.prototype.toString.call(obj) === '[object Array]';
-  }
-
-  /**
-   * 通过请求和应答的proto计算出command值
-   * 
-   * @param {string} proto_request 
-   * @param {string} proto_response 
-   * @returns number
-   */
-  function getCommandFromProto(proto_request, proto_response) {
-    proto_response = proto_response || 'MsgExpress.CommonResponse'
-    const proto_str = proto_request + '-' + proto_response
-    if (commandCache[proto_str]) {
-      return commandCache[proto_str]
-    }
-
-    for (let i = 0, appCount = AppList.length; i < appCount; i++) {
-      const app = AppList[i]
-      const header = app['$']
-      if (!header || !app['function']) {
-        continue;
-      }
-
-      for (let j = 0, funcCount = app['function'].length; j < funcCount; j++) {
-        const func = app['function'][j]
-        const funcObj = func['$']
-        if (funcObj) {
-          if (funcObj.request === proto_request && funcObj.response === proto_response) {
-            const val = (parseInt(header.id, 10) << 20) | parseInt(funcObj.id, 10)
-            commandCache[proto_str] = val
-            return val
-          }
-        }
-      }
-    }
-    console.error('getCommandFromProto error, request:' + proto_response + ', response:' + proto_response)
-    return 0
-  }
-
-  /**
-   * 根据command值，获取request和response的proto名字
-   * 
-   * @param {number} cmd 
-   * @returns object { request: string, response: string }
-   */
-  function getProtoFromCommand(cmd) {
-    const appId = cmd >> 20
-    const funcId = (cmd - ((cmd >> 20) << 20))
-    for (let i = 0, appCount = AppList.length; i < appCount; i++) {
-      const app = AppList[i]
-      const header = app['$']
-      if (!header || !app['function']) {
-        continue
-      }
-
-      if (parseInt(header.id, 10) !== appId) {
-        continue
-      }
-
-      for (let j = 0, funcCount = app['function'].length; j < funcCount; j++) {
-        const func = app['function'][j]
-        const funcObj = func['$']
-        if (funcObj) {
-          if (parseInt(funcObj.id, 10) === funcId) {
-            return {
-              request: funcObj.request,
-              response: funcObj.response
-            }
-          }
-        }
-      }
-    }
-    console.error('getProtoFromCommand error, cmd:' + cmd)
-    return null
-  }
-
-  /**
-   * 根据proto名字获取所在的文件名
-   * 
-   * @param {string} proto 
-   * @returns string
-   */
-  function getProtoFilename(proto) {
-    const prefix = proto.substring(0, proto.indexOf('.'))
-    for (let file of ProtoFileList) {
-      if (file.package === prefix) {
-        return file.filename
-      }
-    }
-    console.error('get proto filename error, proto:' + proto)
-    return ''
-  }
-
   class AppClient {
-    constructor() {
+    constructor(cmdParse) {
+      this._cmdParse = cmdParse;
       this._publishCallback = null
       this._subIdStart = 123
       this._clientName = 'test'
@@ -302,7 +187,7 @@
             // proto中的协议名，新的订阅方式，如：StockServer.StockDataRequest, StockServer.StockDataResponse
             const arr = protoList[i].split(',');
             if (arr.length === 2) {
-              cmd = getCommandFromProto(arr[0].trim(), arr[1].trim());
+              cmd = this._cmdParse.getCommandFromProto(arr[0].trim(), arr[1].trim());
             } else {
               console.error('subscribe params error', protoList[i]);
             }
@@ -343,7 +228,16 @@
      * @returns promise protobuf解析后的消息
      * @memberof AppClient
      */
-    parsePublishMessage(protoFilename, jsonContent) {
+    parseOldPublishMessage(protoFilename, jsonContent) {
+      // const msgExpress = {
+      //   KEY_UUID: 1,KEY_AUTH: 2,KEY_ADDR: 3,KEY_NAME: 4,KEY_TYPE: 5,KEY_GROUP: 6,
+      //   KEY_IP: 7,KEY_STARTTIME: 8,KEY_LOGINTIME: 9,KEY_SERVICE: 10,KEY_HBTIME: 20,
+      //   KEY_CPU: 21,KEY_TOPMEM: 22,KEY_MEM: 23,KEY_CSQUEUE: 24,KEY_CRQUEUE: 25,
+      //   KEY_QUEUELENGTH: 29,KEY_RECVREQUEST: 30,KEY_SENTREQUEST: 31,KEY_RECVRESPONSE: 32,
+      //   KEY_SENTRESPONSE: 33,KEY_RECVPUBLISH: 34,KEY_SENTPUBLISH: 35,KEY_RECVREQUESTB: 36,
+      //   KEY_SENTREQUESTB: 37,KEY_RECVRESPONSEB: 38,KEY_SENTRESPONSEB: 39,KEY_RECVPUBLISHB: 40,
+      //   KEY_SENTPUBLISHB: 41,KEY_LOGLEVEL: 61,KEY_LOGDATA: 62, KEY_TIME: 11 ,KEY_BROKER:12
+      // };
       return new Promise((resolve, reject) => {
         if (jsonContent.length < 2) {
           return reject('json content error')
@@ -379,7 +273,7 @@
      * @memberof AppClient
      */
     post(protoRequest, protoResponse, requestObj) {
-      return this.postProto(getProtoFilename(protoRequest), protoRequest, protoResponse, requestObj)
+      return this.postProto(this._cmdParse.getProtoFilename(protoRequest), protoRequest, protoResponse, requestObj)
     }
 
     /**
@@ -394,7 +288,7 @@
      */
     postProto(protoFilename, protoRequest, protoResponse, requestObj) {
       return new Promise((resolve, reject) => {
-        const cmd = getCommandFromProto(protoRequest, protoResponse)
+        const cmd = this._cmdParse.getCommandFromProto(protoRequest, protoResponse)
         if (!cmd) {
           console.error('command error, request:' + protoRequest + ', response:' + protoResponse)
           return reject()
@@ -459,7 +353,7 @@
      * @memberof AppClient
      */
     dispatchPublishMessage(topic, content) {
-      const proto = getProtoFromCommand(topic)
+      const proto = this._cmdParse.getProtoFromCommand(topic)
       if (!proto || !content || !content.length) {
         console.log('get proto from command failed, topic:' + topic + ',content:' + content)
         return
@@ -476,7 +370,7 @@
         this.onPublishCallback(data)
       } else {
         data.old = false;
-        return databus.buildProtoObject(getProtoFilename(proto.request), proto.request).then(Msg => {
+        return databus.buildProtoObject(this._cmdParse.getProtoFilename(proto.request), proto.request).then(Msg => {
           try {
             data.content = Msg.decode(content)
             this.onPublishCallback(data)
@@ -487,5 +381,84 @@
       }
     }
   }
-  return new AppClient();
+
+  function isArray(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+  }
+
+  const CmdParse = function () {
+    this.commandCache = {}
+  }
+
+  CmdParse.prototype.getCommandFromProto = function (proto_request, proto_response) {
+    proto_response = proto_response || 'MsgExpress.CommonResponse'
+    const proto_str = proto_request + '-' + proto_response
+    if (this.commandCache[proto_str]) {
+      return this.commandCache[proto_str]
+    }
+
+    for (let i = 0, appCount = cbusCommand.AppList.length; i < appCount; i++) {
+      const app = cbusCommand.AppList[i]
+      const header = app['$']
+      if (!header || !app['function']) {
+        continue;
+      }
+
+      for (let j = 0, funcCount = app['function'].length; j < funcCount; j++) {
+        const func = app['function'][j]
+        const funcObj = func['$']
+        if (funcObj) {
+          if (funcObj.request === proto_request && funcObj.response === proto_response) {
+            const val = (parseInt(header.id, 10) << 20) | parseInt(funcObj.id, 10)
+            this.commandCache[proto_str] = val
+            return val
+          }
+        }
+      }
+    }
+    console.error('getCommandFromProto error, request:' + proto_response + ', response:' + proto_response)
+    return 0
+  }
+  CmdParse.prototype.getProtoFromCommand = function (cmd) {
+    const appId = cmd >> 20
+    const funcId = (cmd - ((cmd >> 20) << 20))
+    for (let i = 0, appCount = cbusCommand.AppList.length; i < appCount; i++) {
+      const app = cbusCommand.AppList[i]
+      const header = app['$']
+      if (!header || !app['function']) {
+        continue
+      }
+
+      if (parseInt(header.id, 10) !== appId) {
+        continue
+      }
+
+      for (let j = 0, funcCount = app['function'].length; j < funcCount; j++) {
+        const func = app['function'][j]
+        const funcObj = func['$']
+        if (funcObj) {
+          if (parseInt(funcObj.id, 10) === funcId) {
+            return {
+              request: funcObj.request,
+              response: funcObj.response
+            }
+          }
+        }
+      }
+    }
+    console.error('getProtoFromCommand error, cmd:' + cmd)
+    return 0;
+  }
+  CmdParse.prototype.getProtoFilename = function (proto) {
+    const prefix = proto.substring(0, proto.indexOf('.'))
+    for (let file of cbusCommand.ProtoFileList) {
+      if (file.package === prefix) {
+        return file.filename
+      }
+    }
+    console.error('get proto filename error, proto:' + proto)
+    return ''
+  }
+
+  return new AppClient(new CmdParse());
 });
