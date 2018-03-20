e8aaccb6bac873a7281d24fe9a9dc75c7334d8c9
tujiaw <tujiaw@163.com>
2018-03-20 14:58:03 +0800

    add

--- src/databus/cbusCore.js
+++ src/databus/cbusCore.js
@@ -2,33 +2,28 @@
   /* CommonJS */
   if (typeof require === 'function' && typeof module === "object" && module && module["exports"])
     module['exports'] = (function () {
-      var cbusPackage = require('./cbusPackage');
       var ProtoBuf = require("protobufjs");
       var Long = require("long");
       ProtoBuf.util.Long = Long;
       ProtoBuf.configure();
-      return factory(ProtoBuf, require('bytebuffer'), cbusPackage);
+      return factory(ProtoBuf, require('bytebuffer'), require('pako'));
     })();
   /* Global */
   else
-    global["cbusCore"] = factory(
-      global.protobuf,
-      global.dcodeIO.ByteBuffer,
-      global.cbusPackage
-    );
-})(this, function (ProtoBuf, ByteBuffer, cbusPackage) {
-  var global = this;                        // 当前环境
-  var ws = undefined;                       // WebSocket
-  var serial = 65536;                       // 发送消息的起始序号
-  var protobufBuilders = {};                // protobuf解析缓存
+    global["cbusCore"] = factory(global.protobuf, global.dcodeIO.ByteBuffer, global.pako);
+})(this, function (ProtoBuf, ByteBuffer, pako) {
+  var global = this; // 当前环境
+  var ws = undefined; // WebSocket
+  var serial = 65536; // 发送消息的起始序号
+  var protobufBuilders = {}; // protobuf解析缓存
 
-  var pushDataFactory = undefined;          // 处理推送的消息
-  var wsurl = '';                           // 地址
-  var PREFIX_DATABUS = "DATABUS";           // 消息发送序号前缀
-  var PROTO_FILE_DIR = '/protobuf/';        // proto文件所在目录
-  var reconnectAttempts = 0;                // 尝试重连次数
-  var reconnectIntervalSecond = 0;          // 重连间隔
-  var responseTimeoutSecond = 0;            // 应答超时时间
+  var pushDataFactory = undefined; // 处理推送的消息
+  var wsurl = ''; // 地址
+  var PREFIX_DATABUS = "DATABUS"; // 消息发送序号前缀
+  var PROTO_FILE_DIR = '/protobuf/'; // proto文件所在目录
+  var reconnectAttempts = 0; // 尝试重连次数
+  var reconnectIntervalSecond = 0; // 重连间隔
+  var responseTimeoutSecond = 0; // 应答超时时间
   var settings = {
     onConnectSuccess: undefined,
     onConnectError: undefined,
@@ -65,7 +60,10 @@
 
     // 订阅方法，返回订阅event标识符
     sub(evt, fn) {
-      const obj = { fn: fn, time: new Date().getTime() };
+      const obj = {
+        fn: fn,
+        time: new Date().getTime()
+      };
       if (this.subscribers[evt]) {
         this.subscribers[evt].push(obj);
       } else {
@@ -108,6 +106,210 @@
     }
   }
 
+  const cbusPackage = (function (ByteBuffer, pako) {
+    var pack = function () {
+      this.flag1 = pack.PACKAGE_START;
+      this.flag2 = pack.PACKAGE_START;
+      this.version = 1;
+      this.type = pack.REQUEST;
+      this.off = pack.SIZE_OF_HEAD;
+      this.options = 0;
+      this.codeinfo = 0;
+      this.reserve1 = 0;
+      this.serialNum = 0;
+      this.bodysize = 0;
+      this.srcaddr = 0;
+      this.dstaddr = 0;
+      this.command = 0;
+      this.body = null;
+      this.shortCode = 0;
+    };
+    pack.prototype.setSerialNumber = function (serialNum) {
+      this.serialNum = serialNum;
+    };
+    pack.prototype.getSerialNumber = function () {
+      return this.serialNum;
+    };
+    pack.prototype.setBody = function (body) {
+      this.body = body;
+    };
+    pack.prototype.getBody = function () {
+      return this.body;
+    };
+    pack.prototype.setCommand = function (command) {
+      this.command = command;
+    };
+    pack.prototype.getCommand = function () {
+      return this.command;
+    };
+    pack.prototype.getOffset = function () {
+      return this.off;
+    };
+    pack.prototype.getBodySize = function () {
+      return this.bodysize;
+    };
+    pack.prototype.setBodySize = function (bodysize) {
+      this.bodysize = bodysize;
+    };
+    pack.prototype.getIsZip = function () {
+      return (this.codeinfo & 0x1) === 1;
+    };
+    pack.prototype.getType = function () {
+      return this.type;
+    };
+
+    pack.prototype.isPublishNewMsg = function () {
+      if (this.options && (((this.options >> 7) & 1) === 1)) {
+        return true
+      }
+      return false
+    }
+
+    pack.encodePackage = function (serialNum, command, body) {
+      var pk = new pack();
+      pk.setSerialNumber(serialNum);
+      pk.setCommand(command);
+      pk.setBody(body);
+      pk.setBodySize(body.limit);
+
+      var buffer = new ByteBuffer(pack.SIZE_OF_HEAD + pk.getBodySize());
+      buffer.writeByte(pk.flag1);
+      buffer.writeByte(pk.flag2);
+      buffer.writeByte(pk.version);
+      buffer.writeByte(pk.type);
+      buffer.writeByte(pk.off);
+      buffer.writeByte(pk.options);
+      buffer.writeByte(pk.codeinfo);
+      buffer.writeByte(pk.reserve1);
+      buffer.writeInt(pk.serialNum);
+      buffer.writeInt(pk.bodysize);
+      buffer.writeInt(pk.srcaddr);
+      buffer.writeInt(pk.dstaddr);
+      buffer.writeInt(pk.command);
+      buffer.writeInt16(pk.shortCode);
+      if (pk.body != null) {
+        for (var i = 0; i < pk.body.limit; i++) {
+          buffer.writeByte(pk.body.readByte());
+        }
+      }
+      buffer.offset = 0;
+      return buffer;
+    }
+
+    pack.decodePackage1 = function (buffer) {
+      var packages = [];
+      while (buffer.remaining() > 0) {
+        // read util 'P'
+        if (!(buffer.readByte() === pack.PACKAGE_START && buffer.readByte() === pack.PACKAGE_START)) {
+          continue;
+        }
+        var start = buffer.offset - 2;
+        var headerBytes = buffer.copy(start, start + pack.SIZE_OF_HEAD);
+        buffer.skip(pack.SIZE_OF_HEAD - 2);
+        var header = pack.decodeHeader(headerBytes);
+        packages.push(header);
+
+        if (header.getOffset() - pack.SIZE_OF_HEAD > 0) {
+          buffer.skip(header.getOffset() - pack.SIZE_OF_HEAD);
+        }
+        var bodySize = header.getBodySize();
+        var bodyStart = buffer.offset;
+        var bodyBytes = buffer.copy(bodyStart, bodyStart + bodySize);
+        buffer.skip(bodySize);
+        if (!header.getIsZip()) {
+          header.body = bodyBytes;
+        } else {
+          header.body = ByteBuffer.wrap(pako.inflate(new Uint8Array(bodyBytes.toArrayBuffer())));
+        }
+      }
+      return packages;
+    };
+
+    pack.decodePackageInternal = function (packages, buffer) {
+      var i = 0;
+      while (buffer.remaining() > 0) {
+        if (buffer.remaining < pack.SIZE_OF_HEAD) {
+          break;
+        }
+        // read util 'P'
+        if (!(buffer.readByte() === pack.PACKAGE_START && buffer.readByte() === pack.PACKAGE_START)) {
+          continue;
+        }
+        var start = buffer.offset - 2;
+        var headerBytes = buffer.copy(start, start + pack.SIZE_OF_HEAD);
+        buffer.skip(pack.SIZE_OF_HEAD - 2);
+        var header = pack.decodeHeader(headerBytes);
+        var offset = header.getOffset() - pack.SIZE_OF_HEAD;
+        if (offset > 0) {
+          buffer.skip(offset);
+        }
+        var bodySize = header.getBodySize();
+        var bodyStart = buffer.offset;
+        var bodyBytes = buffer.copy(bodyStart, bodyStart + bodySize);
+        if (buffer.remaining < bodySize) {
+          break;
+        }
+        buffer.skip(bodySize);
+        if (!header.getIsZip()) {
+          header.body = bodyBytes;
+        } else {
+          header.body = ByteBuffer.wrap(pako.inflate(new Uint8Array(bodyBytes.toArrayBuffer())));
+        }
+        i += (pack.SIZE_OF_HEAD + offset + bodySize);
+        packages.push(header);
+      }
+      return i;
+    }
+
+    pack.decodePackage = function (buffer) {
+      var packages = [];
+      if (buffer === undefined || buffer.remaining() === 0) {
+        return packages;
+      }
+      if (recData !== undefined && recData.remaining() > 0) {
+        var buffers = [buffer, recData];
+        var newBuffer = ByteBuffer.concat(buffers);
+        recData = newBuffer;
+      } else {
+        recData = buffer;
+      }
+      recData.mark();
+      var i = this.decodePackageInternal(packages, recData);
+      recData.reset();
+      if (i > 0) {
+        recData.skip(i);
+      }
+      return packages;
+    };
+
+    pack.decodeHeader = function (buffer) {
+      var p = new pack();
+      p.flag1 = buffer.readByte();
+      p.flag2 = buffer.readByte();
+      p.version = buffer.readByte();
+      p.type = buffer.readByte();
+      p.off = buffer.readByte();
+      p.options = buffer.readByte();
+      p.codeinfo = buffer.readByte();
+      p.reserve1 = buffer.readByte();
+      p.serialNum = buffer.readInt();
+      p.bodysize = buffer.readInt();
+      p.srcaddr = buffer.readInt();
+      p.dstaddr = buffer.readInt();
+      p.command = buffer.readInt();
+      p.shortCode = buffer.readInt16();
+      return p;
+    };
+
+    pack.PACKAGE_START = 80;
+    pack.REQUEST = 1;
+    pack.RESPONSE = 2;
+    pack.Publish = 3;
+    pack.SIZE_OF_HEAD = 0x1e;
+    var recData = undefined;
+    return pack;
+  })(ByteBuffer, pako);
+
   var cbusCore = {
     observer: new Observer(),
     close: function () {
@@ -126,13 +328,13 @@
       }
       return -1
     },
-    setReconnectIntervalSecond: function(second) {
+    setReconnectIntervalSecond: function (second) {
       reconnectIntervalSecond = second;
     },
-    setResponseTimeoutSecond: function(second) {
+    setResponseTimeoutSecond: function (second) {
       responseTimeoutSecond = second;
     },
-    setConnectOptions: function(options) {
+    setConnectOptions: function (options) {
       for (var p in options) {
         if (settings[p]) {
           delete settings[p];
@@ -140,7 +342,7 @@
         settings[p] = options[p];
       }
     },
-    getUrl: function() {
+    getUrl: function () {
       return wsurl;
     },
     connect: function (url, options) {
